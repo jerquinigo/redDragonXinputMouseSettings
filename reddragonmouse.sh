cb09d981f549343029313fc13c64a1b5649a5054
jerquinigo <jonathanerquinigo@pursuit.org>
2025-06-20 14:38:00 -0400

    main mouse settings

---
+++ reddragonmouse.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+mouse_name="Compx 3-Mode Mouse"
+ 
+#mouse_line=$(xinput | grep -i  "$mouse_name" | grep -v "Consumer Control")
+mouse_line=$(xinput list | grep -i "$mouse_name" | grep 'pointer' | grep -v 'Consumer Control' | grep -v 'keyboard')
+
+
+mouse_id=$(echo "$mouse_line" | sed -n 's/.*id=\([0-9]*\).*/\1/p')
+
+echo "Mouse ID: $mouse_id"
+
+xinput set-button-map "$mouse_id" 1 2 3 4 5 6 7 1 1
+
